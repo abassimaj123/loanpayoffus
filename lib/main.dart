1be0125a335bcfd1f43218fb304024657a9a0264
abassimaj123 <abassimaj123@users.noreply.github.com>
2026-06-25 11:54:45 -0400

    fix(ads): threshold 7/5

--- lib/main.dart
+++ lib/main.dart
@@ -80,6 +80,8 @@ final adService = CalcwiseAdService(
     bannerAndroid: AdConfig.bannerAndroid,
     interstitialAndroid: AdConfig.interstitialAndroid,
     rewardedAndroid: AdConfig.rewardedAndroid,
+    calcThreshold: 7,
+    cooldownMinutes: 5,
   ),
   freemium: freemiumService,
   analytics: AnalyticsService.instance,
